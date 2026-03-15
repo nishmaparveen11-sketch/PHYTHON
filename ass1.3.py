168f90a866672f758672275ebd391a877629e7fa
nishmaparveen11-sketch <nishmaparveen11@gmail.com>
2026-03-15 19:03:53 +0530

    Add files via upload

---
+++ ass1.3.py
@@ -0,0 +1,20 @@
+a=43
+b=34
+print("add=",a+b)
+print("sub=",a-b)
+
+d=53
+e=97
+print("mul=",d*e)
+
+f=4
+g=4
+print("power=",f**g)
+
+h=76
+i=31
+print("modulo=",h%i)
+print("floor div=",h//i)
+print("float",h/i)
+
+
